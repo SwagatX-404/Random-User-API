8636f656c334e03696fe46578a8506f3e4d90e0e
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-11-01 23:53:03 +0530

    implement user data fetching and display functionality

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 async function getUser(){
     try{
+        const res = await fetch("https://randomuser.me/api/");
+        const data = await res.json();
+        const user = data.results[0];
+
+        document.getElementById("user-img").src = user.picture.large;
+        document.getElementById("user-name").innerText = user.name.first;
+        document.getElementById("user-email").innerText = user.email;
 
     }catch(error){
         console.error("Error Fetching Data: ", error);
